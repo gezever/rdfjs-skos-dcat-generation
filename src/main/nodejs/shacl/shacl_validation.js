f9bbbdbda4f04305bbdc2eb1c67f6a76c9a613fd
gezever <gezever@skynet.be>
2024-01-10 17:10:12 +0100

    test comunica federated query test rdf-sparql-builder

--- src/main/nodejs/shacl/shacl_validation.js
+++ src/main/nodejs/shacl/shacl_validation.js
@@ -2,14 +2,10 @@ import rdf from '@zazuko/env-node'
 import SHACLValidator from 'rdf-validate-shacl'
 
 async function validate(shapes, data) {
-    //const shapes = await rdf.dataset().import(rdf.fromFile('my-shapes.ttl'))
-    //const data = await rdf.dataset().import(rdf.fromFile('my-data.ttl'))
     console.log("Validation");
     const validator = new SHACLValidator(shapes, { factory: rdf })
     const report = await validator.validate(data)
 
-    // Check conformance: `true` or `false`
-    console.log(report.conforms)
 
     for (const result of report.results) {
         // See https://www.w3.org/TR/shacl/#results-validation-result for details
@@ -24,6 +20,8 @@ async function validate(shapes, data) {
 
     // Validation report as RDF dataset
     console.log(await report.dataset.serialize({ format: 'text/n3' }))
+    // Check conformance: `true` or `false`
+    // console.log(report.conforms)
     return report.conforms;
 }
 export default validate
